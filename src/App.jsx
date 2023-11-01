6384d68a1d7fc8f25491633c0c8729a6949d866a
Rodrigo <rtf.franca@gmail.com>
2023-10-31 23:43:26 -0300

    Rendering list of friends

---
+++ src/App.jsx
@@ -0,0 +1,69 @@
+import React from "react";
+import "./index.css";
+
+const initialFriends = [
+  {
+    id: 118836,
+    name: "Clark",
+    image: "https://i.pravatar.cc/48?u=118836",
+    balance: -7,
+  },
+  {
+    id: 933372,
+    name: "Sarah",
+    image: "https://i.pravatar.cc/48?u=933372",
+    balance: 20,
+  },
+  {
+    id: 499476,
+    name: "Anthony",
+    image: "https://i.pravatar.cc/48?u=499476",
+    balance: 0,
+  },
+];
+
+export default function App() {
+  return (
+    <div className="app">
+      <div className="sidebar">
+        <FriendsList />
+      </div>
+    </div>
+  );
+}
+
+function FriendsList() {
+  const friends = initialFriends;
+
+  return (
+    <ul>
+      {friends.map((friend) => (
+        <Friend friendObj={friend} key={friend.id} />
+      ))}
+    </ul>
+  );
+}
+
+function Friend({ friendObj }) {
+  return (
+    <li>
+      <img src={friendObj.image} alt={friendObj.name} />
+      <h3>{friendObj.name}</h3>
+      {friendObj.balance < 0 && (
+        <p className="red">
+          You owe {friendObj.name} ${Math.abs(friendObj.balance)}
+        </p>
+      )}
+      {friendObj.balance > 0 && (
+        <p className="green">
+           {friendObj.name} owes you ${Math.abs(friendObj.balance)}
+        </p>
+      )}
+      {friendObj.balance === 0 && (
+        <p>
+          You and {friendObj.name} are even</p>
+      )}
+      <button className="button">Select</button>
+    </li>
+  );
+}
