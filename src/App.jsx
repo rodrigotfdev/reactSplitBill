50f7b835d89430194acd9db03cfb3270ee3352d1
Rodrigo <rtf.franca@gmail.com>
2023-11-02 14:23:47 -0300

    Creating controlled elements

--- src/App.jsx
+++ src/App.jsx
@@ -39,7 +39,7 @@ export default function App() {
   function handleSelection(friend) {
     // setSelectedFriend(friend);
     setSelectedFriend((cur) => (cur?.id === friend.id ? null : friend));
-    setShowAddFriend(false)
+    setShowAddFriend(false);
   }
 
   return (
@@ -158,20 +158,37 @@ function FormAddFriend({ onAddFriend }) {
 }
 
 function FormSplitBill({ selectedFriend }) {
+  const [bill, setBill] = useState("");
+  const [paidByUser, setPaidByUser] = useState("");
+  const paidByFriend = bill ? bill - paidByUser : "";
+  const [whosIsPaying, setWhoIsPaying] = useState("user");
+
   return (
     <form action="" className="form-split-bill">
       <h2>Split a bill with {selectedFriend.name} </h2>
+
       <label>💸 Bill value</label>
-      <input type="text" />
+      <input
+        type="text"
+        value={bill}
+        onChange={(e) => setBill(Number(e.target.value))}
+      />
 
       <label>👦🏻 Your expense</label>
-      <input type="text" />
+      <input
+        type="text"
+        value={paidByUser}
+        onChange={(e) => setPaidByUser(Number(e.target.value) > bill ? paidByUser : Number(e.target.value))}
+      />
 
       <label>👩🏻 {selectedFriend.name}'s expense</label>
-      <input type="text" />
+      <input type="text" disabled value={paidByFriend} />
 
       <label>🤑 Who's paying the bill</label>
-      <select>
+      <select
+        value={whosIsPaying}
+        onChange={(e) => setWhoIsPaying(e.target.value)}
+      >
         <option value="user">You</option>
         <option value="friend">{selectedFriend.name}</option>
       </select>
